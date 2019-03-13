d89b3353483be0a282b05877c1eb7ae8e5c90dfc
adamcglee <adamcglee@gmail.com>
2019-03-12 19:12:55 -0700

    completed splash page and made some functionality changes

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './splash.css';
+import splashimage from './images/PhotoeyeLogo.png';
 
 const Splash = (props) => {
     return ( 
-        <div>
-            Splash Page
-            <button onClick={props.splashButton}>Test</button>
+        <div className="splash-container">
+            <img src={splashimage} alt="photoeye logo" className="logo-image"/>
+            <p className="motto">Come train your eye and perfect your craft...</p>
+            <div className="enter-button" onClick={props.splashButton}>Enter</div>
         </div>
      );
 }
