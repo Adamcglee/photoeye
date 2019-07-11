cb79d752512115beeeb09a90ec5ed8bad8c48fb9
adamcglee <adamcglee@gmail.com>
2019-07-10 22:02:38 -0700

    changed layout and color scheme, also added more into to splash page.

--- src/components/splash.js
+++ src/components/splash.js
@@ -7,6 +7,13 @@ const Splash = (props) => {
         <div className="splash-container">
             <img src={splashimage} alt="photoeye logo" className="logo-image"/>
             <p className="motto">Train your eye and perfect your craft...</p>
+            <div>
+                <h6>By: <a href="https://adamlee.dev">Adam Lee</a></h6>
+                <p><a href="https://github.com/Adamcglee/photoeye">Github Repo</a></p>
+                <a href="https://github.com/Adamcglee"><i class="fab fa-github"></i></a>
+                <a href="https://www.linkedin.com/in/adamcglee/"><i class="fab fa-linkedin"></i></a>
+                <a href="https://drive.google.com/file/d/1laxkzXChH-a-O4ybtUslVucNtuCTsWsZ/view?usp=sharing"><i class="fas fa-file-alt"></i></a>
+            </div>
             <div className="enter-button" onClick={props.splashButton}>Enter</div>
         </div>
      );
